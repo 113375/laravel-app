da9732feec683e7ecdb42aba9f7488f54d85936a
113375 <knyazevdima05@gmail.com>
2023-07-20 17:41:16 +0300

    work

--- laravel-react/src/components/DefaultLayout.jsx
+++ laravel-react/src/components/DefaultLayout.jsx
@@ -1,11 +1,11 @@
 import { Navigate, Outlet } from "react-router-dom";
-import { useStateContext } from "../contexts/ContextProvider";
+// import { useStateContext } from "../contexts/ContextProvider";
 
 export default function DefaultLayout(){
-    const {user, token} = useStateContext()
-    if (!token){
-        return <Navigate to="/login"/>
-    }
+    // const {user, token} = useStateContext()
+    // if (!token){
+    //     return <Navigate to="/login"/>
+    // }
     return (
         <div>
             <Outlet/>
--- laravel-react/src/components/GuestsLayout.jsx
+++ laravel-react/src/components/GuestsLayout.jsx
@@ -1,11 +1,11 @@
 import { Navigate, Outlet } from "react-router-dom";
-import { useStateContext } from "../contexts/ContextProvider";
+// import { useStateContext } from "../contexts/ContextProvider";
 
 export default function GuestsLayout(){
-    const {token} = useStateContext();
-    if (token){
-        return <Navigate to={'/'}/>
-    }
+    // const {token} = useStateContext();
+    // if (token){
+    //     return <Navigate to={'/'}/>
+    // }
     return (
         <div>
            <div>
